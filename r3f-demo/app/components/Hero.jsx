e2086d3f2a9e625f8d86c8f273d5a7056d53fbf1
Mustapha Osman <wnfynn@gmail.com>
2026-01-20 08:24:45 +0000

    using framer motion

--- r3f-demo/app/components/Hero.jsx
+++ r3f-demo/app/components/Hero.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { motion } from "motion/react";
+
+const animation = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      ease: "easeInOut",
+      duration: 2,
+      staggerChildren: 2,
+      delayChildren: 3,
+    },
+  },
+};
+const itemAnimation = {
+  hidden: { opacity: 0, y: -200 },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 const Hero = () => {
-  return <div className="h-screen">Hero</div>;
+  const fruits = ["orange", "apple", "mango", "banana", "papaya"];
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <motion.ul variants={animation} initial={"hidden"} animate={"visible"}>
+        {fruits.map((item, i) => {
+          return (
+            <motion.li
+              key={i}
+              variants={itemAnimation}
+              initial={"hidden"}
+              animate={"visible"}
+            >
+              {item}
+            </motion.li>
+          );
+        })}
+      </motion.ul>
+    </div>
+  );
 };
 
 export default Hero;
--- r3f-demo/app/page.js
+++ r3f-demo/app/page.js
@@ -1,21 +1,19 @@
 "use client";
 import React from "react";
 import Hero from "./components/Hero";
-import { ServicesModel } from "./components/ServicesModel";
-import { Canvas } from "@react-three/fiber";
-import { Bounds, Environment, OrbitControls } from "@react-three/drei";
-
+import { motion } from "motion/react";
 const page = () => {
   return (
     <div>
       <Hero />
-      <div className="w-full h-screen">
-        <Canvas className="w-full h-screen border-red-700 border-2">
-          <Environment preset="sunset" />
-          <ambientLight />
-          <ServicesModel scale={2} />
-        </Canvas>
-      </div>
+      <motion.div
+        className="w-full h-screen flex justify-center items-center"
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2 }}
+      >
+        <h1>Hello World</h1>
+      </motion.div>
     </div>
   );
 };
