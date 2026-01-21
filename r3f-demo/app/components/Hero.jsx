b4f5e332fdb38047e2cb6261f2397e49f9bf5b5f
Mustapha Osman <wnfynn@gmail.com>
2026-01-21 16:58:38 +0000

    3d glass effect with three js

--- r3f-demo/app/components/Hero.jsx
+++ r3f-demo/app/components/Hero.jsx
@@ -1,45 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
 import { motion } from "motion/react";
-
-const animation = {
-  hidden: { opacity: 0 },
-  visible: {
-    opacity: 1,
-    transition: {
-      ease: "easeInOut",
-      duration: 2,
-      staggerChildren: 2,
-      delayChildren: 3,
-    },
-  },
-};
-const itemAnimation = {
-  hidden: { opacity: 0, y: -200 },
-  visible: {
-    opacity: 1,
-    y: 0,
-  },
-};
+import { MeshTransmissionMaterial, Text, useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useControls } from "leva";
 
 const Hero = () => {
-  const fruits = ["orange", "apple", "mango", "banana", "papaya"];
+  const mesh = useRef();
+  const { nodes } = useGLTF("/models/torus.glb");
+  const { viewport } = useThree();
+
+  useFrame((state, delta) => {
+    mesh.current.rotation.x += delta + 0.009;
+  });
+
+  const controls = useControls("Glass", {
+    thickness: { value: 0.5, min: 0, max: 5, step: 0.01 },
+    roughness: { value: 0, min: 0, max: 1, step: 0.01 },
+    transmission: { value: 1, min: 0, max: 1, step: 0.01 },
+    ior: { value: 1.5, min: 1, max: 2.5, step: 0.01 },
+    chromaticAberration: { value: 0.02, min: 0, max: 1, step: 0.001 },
+    anisotropy: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    distortion: { value: 0, min: 0, max: 1, step: 0.01 },
+    distortionScale: { value: 0.5, min: 0, max: 1, step: 0.01 },
+    temporalDistortion: { value: 0, min: 0, max: 1, step: 0.01 },
+    attenuationDistance: { value: 0.5, min: 0, max: 10, step: 0.1 },
+    attenuationColor: "#ffffff",
+    color: "#ffffff",
+    backside: { value: true },
+  });
+
   return (
-    <div className="h-screen w-full flex justify-center items-center">
-      <motion.ul variants={animation} initial={"hidden"} animate={"visible"}>
-        {fruits.map((item, i) => {
-          return (
-            <motion.li
-              key={i}
-              variants={itemAnimation}
-              initial={"hidden"}
-              animate={"visible"}
-            >
-              {item}
-            </motion.li>
-          );
-        })}
-      </motion.ul>
-    </div>
+    <group scale={viewport.width / 8}>
+      <Text scale={3.5} position={[0, 0, -3]}>
+        Hello
+      </Text>
+      <mesh {...nodes.Torus} ref={mesh}>
+        <MeshTransmissionMaterial {...controls} />
+      </mesh>
+    </group>
   );
 };
 
--- r3f-demo/app/page.js
+++ r3f-demo/app/page.js
@@ -2,18 +2,18 @@
 import React from "react";
 import Hero from "./components/Hero";
 import { motion } from "motion/react";
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import { GridHelper } from "three";
 const page = () => {
   return (
-    <div>
-      <Hero />
-      <motion.div
-        className="w-full h-screen flex justify-center items-center"
-        initial={{ opacity: 0, x: -100 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 2 }}
-      >
-        <h1>Hello World</h1>
-      </motion.div>
+    <div className="h-screen w-full bg-black">
+      <Canvas>
+        <Environment files={"/hdri/university_workshop_2k.exr"} />
+        <axesHelper />
+        <gridHelper />
+        <Hero />
+      </Canvas>
     </div>
   );
 };
