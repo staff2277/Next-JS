d4e88b737005628093b610abbec6313074f618d6
Mustapha Osman <wnfynn@gmail.com>
2026-01-24 21:08:55 +0000

    Add pointer move event handler to Home component and refactor Model component imports

--- r3f-gsap-canvas/app/page.js
+++ r3f-gsap-canvas/app/page.js
@@ -3,10 +3,16 @@ import Model from "@/componets/Model";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    function handlePointerMove() {
+      console.log("pointer moved");
+    }
+  });
   return (
-    <div className="h-screen w-full">
+    <div className="h-screen w-full" onPointerMove={handlePointerMove}>
       <Canvas>
         <axesHelper />
         <gridHelper />
--- r3f-gsap-canvas/componets/Model.jsx
+++ r3f-gsap-canvas/componets/Model.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Model = () => {
   const mesh = useRef(null);
+
   return (
     <mesh ref={mesh}>
       <boxGeometry args={[2, 2, 2]} />
